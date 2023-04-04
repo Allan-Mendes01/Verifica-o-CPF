5048e0981f0a53373f7a36a31d4daa6a295cee89
Allan Mendes <107664214+Allan-Mendes01@users.noreply.github.com>
2023-04-04 12:32:09 -0300

    Add files via upload

--- valid_CPF.cpp
+++ valid_CPF.cpp
@@ -13,19 +13,31 @@ scanf("%d", &d);
 scanf("%d", &e);
 scanf("%d", &f);
 printf(". \n");
-
 scanf("%d", &g);
 scanf("%d", &h);
 scanf("%d", &i);
 printf("-\n");
 scanf("%d", &j);
-scanf("%d", &k);
+scanf("%d", &k);.+
 
-v1 = (a * 10 + b * 9 + c * 8 + d * 7 + e * 6 + f * 5 + g * 4 + h * 3 + i * 2)*10 % 11;
+v1 = ((a * 10) + (b * 9) + (c * 8) + (d * 7) + (e * 6) + (f * 5) + (g * 4) + (h * 3) + (i * 2)) % 11;
+
+	if (v1>=2) {
+		v1 = 11-v1;
+	}else {
+		v1 = 0;
+	}
 
 if (v1 == j) {
-	v2 = (a * 11 + b * 10 + c * 9 + d * 8 + e * 7 + f * 6 + g * 5 + h * 4 + i * 3 + j * 2)*10 % 11;
 	
+	v2 = ((a * 11 )+ (b * 10) + (c * 9) + (d * 8) + (e * 7) + (f * 6) + (g * 5) + (h * 4) + (i * 3) + (j * 2)) % 11;
+	
+		if (v2>=2) {
+			v2 = 11 - v2;
+		}else {
+			v2 = 0;
+		}
+		
 	if(v2 == k) {
 		printf("cpf Valido");
 	}
